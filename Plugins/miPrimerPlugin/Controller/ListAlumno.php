fadbb584371a64f1559afa60d17d67e4b7e4f2b0
Carlos Vidal <carlosvsedet@gmail.com>
2024-10-24 16:33:20 +0200

    Subido

--- Plugins/miPrimerPlugin/Controller/ListAlumno.php
+++ Plugins/miPrimerPlugin/Controller/ListAlumno.php
@@ -2,7 +2,10 @@
 
 namespace FacturaScripts\Plugins\miPrimerPlugin\Controller;
 
+use FacturaScripts\Core\Base\DataBase\DataBaseWhere;
 use FacturaScripts\Core\Lib\ExtendedController\ListController;
+use FacturaScripts\Dinamic\Model\Matricula;
+use FacturaScripts\Dinamic\Model\Recibo;
 
 class ListAlumno extends ListController
 {
@@ -40,6 +43,13 @@ class ListAlumno extends ListController
         $this->addOrderBy($viewName, ['fecha'], 'Fecha', 2);
         //$this->addOrderBy($viewName, ['nombre'], 'Nombre');
         $this->addFilterPeriod($viewName, 'date', 'period', 'fecha');
+        $this->addButton($viewName, [
+            'action' => 'generar-recibos',
+            'icon' => 'fas fa-euro',
+            'color' => 'success',
+            'label' => 'Generar Recibos',
+            'type' => 'action',
+        ]);
     }
 
     protected function createMatricula($viewName = "ListMatricula")
@@ -67,6 +77,37 @@ class ListAlumno extends ListController
             case 'ListEvaluacion':
                 $view->loadData();
                 break;
+        }
+    }
+
+    // Este método ejecuta una acción dependiendo del (nombre del action) para ejecutar una función.
+    protected function execPreviousAction($action)
+    {
+        switch ($action) {
+            case 'generar-recibos':
+                $this->actionGenerarRecibos();
+                break;
+        }
+    }
+
+    protected function actionGenerarRecibos()
+    {
+        //TODO: Programar funcionalidad para el botón de generar recibos
+
+        $matriculas = new Matricula();
+        $matriculas = $matriculas->all();
+        $numRecibos = 0;
+        foreach ($matriculas as $matricula) {
+            if ($matricula->fechadebaja == null) {
+                $recibo = new Recibo();
+                $recibo->idalumno = $matricula->idalumno;
+                $recibo->fecha = Date('Y-m-d');
+                $recibo->importe = $matricula->cuota;
+                $recibo->tipo = 'Cuota';
+                if ($recibo->save()) {
+                    $numRecibos++;
+                }
+            }
         }
     }
 }
